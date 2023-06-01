9604e04c751e701b2757f38b5f77ea57746e2e61
waleed-se3fan <waleedsefan24@gmail.com>
2023-06-01 22:05:28 +0300

    adding new feuture and hanling logic

--- lib/app/home/home_cubit/home_cubit.dart
+++ lib/app/home/home_cubit/home_cubit.dart
@@ -6,12 +6,23 @@ import '../../login/login_cubit/login_cubit.dart';
 import '../../signup/signup_cubit/cubit.dart';
 import 'home_states.dart';
 import 'package:translator/translator.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 import 'package:http/http.dart' as http;
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
   static HomeCubit get(context) => BlocProvider.of(context);
+
+  static bool toggle = false;
+
+  static String tvoice =
+      'https://coqui-prod-creator-app-synthesized-samples.s3.amazonaws.com/samples/fc0b3fb0-8344-428d-9775-c9a3e95febfb.wav';
+
+  chgToggle(x) {
+    toggle = x;
+    emit(chgToggleState());
+  }
 
   static String selectedLanguage = 'English';
   static String type = 'en';
@@ -28,18 +39,48 @@ class HomeCubit extends Cubit<HomeState> {
     }
   }
 
-  static String? data;
+  ///////
+
+  ggg() async {
+    AudioPlayer myAudioPlayer = AudioPlayer();
+    await myAudioPlayer.play(tvoice);
+  }
+
+  static var data;
+
+  static var tdata = 'You have a maid who has a sweetheart?';
   Future<String?> getTextData() async {
     try {
-      final response = await http.post(
-          Uri.parse('https://cord0.me/api/signals/simulate'),
-          body: {},
-          headers: {
-            "Accept": "application/json",
-            'authorization': 'Bearer $userToken'
-          });
-      data = await jsonDecode(response.body);
-      return data;
+      if (toggle == true) {
+        final response = await http
+            .post(Uri.parse('https://cord0.me/api/signals/simulate'), body: {
+          'cloned': '1'
+        }, headers: {
+          "Accept": "application/json",
+          'authorization': 'Bearer 164|gQAO3y8REngouxtlSaK2iMClrKdHSoYybuZFoFhD'
+        });
+
+        var mydata = await jsonDecode(response.body);
+        tdata = mydata['text'];
+        tvoice = mydata['audio_url'];
+
+        print(tdata);
+        print('++++++++++++');
+        print(tvoice);
+        print('-----------------');
+
+        return tdata;
+      } else {
+        final response = await http.post(
+            Uri.parse('https://cord0.me/api/signals/simulate'),
+            body: {},
+            headers: {
+              "Accept": "application/json",
+              'authorization': 'Bearer $userToken'
+            });
+        data = await jsonDecode(response.body);
+        return data;
+      }
     } catch (e) {
       print(e.toString());
       return e.toString();
@@ -48,13 +89,20 @@ class HomeCubit extends Cubit<HomeState> {
 
   FlutterTts ftts = FlutterTts();
 
-  tts() async {
-    await ftts.setLanguage("en-US");
-    await ftts.setSpeechRate(0.2); //speed of speech
-    await ftts.setVolume(1.0); //volume of speech
-    await ftts.setPitch(1);
+  //------------------------------- speacking the the output text ----------------------------------//
 
-    var result = await ftts.speak(translatedText);
+  tts() async {
+    if (toggle == true) {
+      AudioPlayer myAudioPlayer = AudioPlayer();
+      await myAudioPlayer.play(tvoice);
+    } else {
+      await ftts.setLanguage("en-US");
+      await ftts.setSpeechRate(0.2); //speed of speech
+      await ftts.setVolume(1.0); //volume of speech
+      await ftts.setPitch(1);
+
+      var result = await ftts.speak(translatedText);
+    }
   }
 
   stoptts() async {
@@ -99,7 +147,7 @@ class HomeCubit extends Cubit<HomeState> {
     emit(StopRecording());
   }
 
-  static String translatedText = 'wait ....';
+  static String translatedText = 'I only doubt—';
   /*  'When I glance over my notes and records of the Sherlock Holmes cases between the years ’82 and ’90, I am faced by so many which present strange and interesting features that it is no easy matter to know which to choose and which to leave.';
  */
 
